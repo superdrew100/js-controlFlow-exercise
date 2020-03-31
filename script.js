0beef02788489463e1637c8a3c61eb5b86dc75b5
Drew Thomasson <superdrew100@Gmail.com>
2020-03-31 08:46:28 -0400

    made changes to script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let i = 2000
+for (let i = 0; i<10;i++){
+    console.log(i)
+}
+
+console.log(i)
+
+let coutner = 10
+for(let i=o; ; < counter ; i++ ) {
+    let charstring= ""
+    for ( let i = 0 ; i < counter; i++ ) {
+        charstring +="#"
+    }
+}
+console.log(charstring)
