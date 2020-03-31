622acb1da196fe24e7774e7a9c087ab5f4e2b67a
Drew Thomasson <superdrew100@Gmail.com>
2020-03-31 09:57:45 -0400

    folling whats being said in class and thinking aloud in the text trying tof igure out whats going on

--- script.js
+++ script.js
@@ -20,5 +20,46 @@ console.log(charstring)
 
 
 
+//going over the answers in class
+//answer for part one
+for (let i = 0; i < 10; i++) {
+    console.log('H H H H H H H H H H')
+}
+//or you could do this
+for(let i = 0; i< 10; i++) {
+    let line = ''
+    for(let i = 0; i< 10; i++){
+        line += 'H'
 
+    }
+    console.log(line)
+}
+//part 2 
+for(let i =0; i < 10; i++){
+    console.log('HHHHHHHHHH')
+}
 
+let line = ''
+for (let i = 0; i< 10; i++) {
+    line += 'H'
+}
+console.log(line)
+
+//askiart triangle part
+
+for(let i =0; i < 8; i++) {
+
+    console.log(i + 1)
+    console.log('H')
+    for(let j = 0; j < (i + 1))
+}
+
+//thinking to myself
+let line = ''
+//this is saying that the value of-
+//line is a empty string
+line += 'H'
+// += is saying take the current value of-
+//line and add 'H' to it, so it would-
+//add add 'H' to the string each time-
+//this command is run
